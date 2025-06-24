e06a14bc441e272d680c20783cac259c0b44e003
Ryan <79425989+agreatbigpileofthings@users.noreply.github.com>
2025-06-24 13:54:00 -0400

    Update !Misc_OneOffs.ps1

--- !Misc_OneOffs.ps1
+++ !Misc_OneOffs.ps1
@@ -1,4 +1,4 @@
 ### Pull ALL GPO Reports for upload to Intune
 Get-GPO -All | ForEach-Object {
-    Get-GPOReport -Name $_.DisplayName -ReportType Xml -Path "C:\GPOReports\$($_.DisplayName).xml"
+    Get-GPOReport -Name $_.DisplayName -ReportType Xml -Path "C:\temp\GPOReports\$($_.DisplayName).xml"
 }
