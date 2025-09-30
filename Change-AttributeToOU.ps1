1a9018eae3089587691c184e296bbb152beb8e30
Ryan <79425989+agreatbigpileofthings@users.noreply.github.com>
2025-09-30 13:12:24 -0400

    Create Change-AttributeToOU.ps1

---
+++ Change-AttributeToOU.ps1
@@ -0,0 +1,17 @@
+<#
+.DESCRIPTION
+   Comb through AD, grab OU, apply to extensionAttribute1
+.EXAMPLE
+   Just Run It
+#>
+
+$Users = Get-ADUser -Filter * -Properties SamAccountName, extensionAttribute1
+foreach($user in $Users){
+    $DistinguishedNames = Get-ADUser -Identity $user | Where-Object DistinguishedName -Like "*OU=Users*" | Select-Object -ExpandProperty DistinguishedName 
+    foreach($name in $DistinguishedNames){
+        $OU = $name.Split("=")[3]
+        $CleanName = $OU.Split(",")[0]
+        write-host "$($user.Name) | $CleanName"
+        Set-ADUser -Identity $user -Add @{"extensionAttribute1"="$CleanName"}
+    }
+}
