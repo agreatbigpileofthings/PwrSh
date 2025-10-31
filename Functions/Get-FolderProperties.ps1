fb6f9f3c70a1d728f1c05e3901656fd200688661
Ryan <79425989+agreatbigpileofthings@users.noreply.github.com>
2025-10-31 09:53:25 -0400

    Create Get-FolderProperties.ps1

---
+++ Functions/Get-FolderProperties.ps1
@@ -0,0 +1,34 @@
+function Get-FolderProperties {
+<#
+.DESCRIPTION
+  Grab folder permissions for folders in parent directory
+   
+.EXAMPLE
+   Get-FolderProperties -FolderPath <path to folder> -Depth 1 -OutputPath <path to dump output csv>
+
+#>
+param(
+    [parameter(Mandatory=$true)]
+    [string]$FolderPath,
+    [parameter(Mandatory=$true)]
+    [int]$Depth,
+    [parameter(Mandatory=$true)]
+    [string]$OutputPath    
+)
+
+$Folders = Get-ChildItem -Path $FolderPath -Depth 1 -Recurse -Force
+$OutputData = @()
+foreach ($Folder in $Folders) {
+    $Acl = Get-Acl -Path $Folder.FullName
+    foreach ($Access in $Acl.Access) {
+        $Properties = [ordered]@{
+            "Folder Name" = $Folder.FullName
+            "Group/User" = $Access.IdentityReference
+            "Permissions" = $Access.FileSystemRights
+            "Inherited" = $Access.IsInherited
+        }
+        $OutputData += New-Object -TypeName PSObject -Property $Properties
+    }
+}
+$OutputData | Export-Csv -Path "$($OutputPath)\FolderPermissionsExport-$(Get-Date -format yyyy-MM-dd).csv" -NoTypeInformation
+}
